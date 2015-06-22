fe006db5f78112eed557606af3ed69d5b0c549ee
min20 <min20@nhnnext.org>
2015-06-23 00:02:15 +0900

    2015.06.22

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-var titleSuffix = 'Megadyne Korea - 제품';
+var titleSuffix = 'Megadyne Korea';
 
 var kinds = {
 	'polyurethane': '폴리우레탄 벨트',
@@ -12,24 +12,48 @@ var kinds = {
 	'special': '특수 벨트'
 };
 
+var pages = {
+	'polyurethane': {
+		'megaflex': 'MEGAFLEX',
+		'megaflat': 'MEGAFLAT',
+		'megalinear': 'MEGALINEAR',
+		'megapower': 'MEGAPOWER',
+		'megarib': 'MEGARIB'
+	},
+	'rubber': {
+		'v-belts': 'V 벨트',
+		'pv-ribbed': 'PV RIBBED 벨트',
+		'megaflat-rubber': 'MEGAFLAT RUBBER',
+		'isoran-synchronous': 'ISORAN SYNCH.'
+	},
+	'pulley': {
+		'pulleys': '풀리',
+		'timing-bars': '타이밍 바',
+		'taper-bushes': '테이퍼 부쉬',
+		'flanges': '플랜지',
+		'clamping-plates': '클램핑 플레이트'
+	},
+	'other': {
+		'conveyor': '컨베이어 벨트',
+		'megablue': 'MEGABLUE',
+		'accu-link': 'ACCU LINK',
+		'megaweld': 'MEGAWELD'
+	},
+	'special': {
+		'rubber': '고무 벨트 주문제작',
+		'polyurethane': '폴리우레탄 주문제작',
+		'cover-properties': '특수 코팅',
+		'cleats': '고마찰'
+	}
+}
+
 router.get('/', function(req, res, next) {
 	var pageScope = {
 		baseUrl: req.baseUrl,
-		title: titleSuffix
+		title: titleSuffix + ' - 제품'
 	};
 
 	res.render('products', pageScope);
-});
-
-router.get('/:kind', function(req, res, next) {
-	var kind = req.params.kind;
-
-	var pageScope = {
-		baseUrl: req.baseUrl,
-		title: titleSuffix + ' - ' + kinds[kind]
-	};
-
-	res.render(path.join('products', kind), pageScope);
 });
 
 router.get('/:kind/:product', function(req, res, next) {
@@ -38,7 +62,7 @@ router.get('/:kind/:product', function(req, res, next) {
 
 	var pageScope = {
 		baseUrl: req.baseUrl,
-		title: titleSuffix + ' - ' + product.toUpperCase()
+		title: pages[kind][product] ? titleSuffix + ' - ' + pages[kind][product] : titleSuffix
 	};
 
 	res.render(path.join('products', kind, product), pageScope);
