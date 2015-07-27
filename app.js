1ecb991bdab37bf4f25da9e9aa9566f63ab4822c
root <root@alfoxpzm.cafe24.com>
2015-07-27 13:20:25 +0900

    2015.07.27

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.locals.pretty = true;
 // MIDDLEWARES
 //===================================================
 app.use(favicon(getDirectory('public/favicon.ico')));
-app.use(morgan('[:date[iso]] :method :status :remote-addr\t:url', {
+app.use(morgan('[:date[iso]] :method :status :remote-addr :url', {
 	skip: function(req, res) {
 		var isSkip = 
 				req.url.search('/images') *
